7b67c27216196e3aad4dcd947bf2a4783079c510
Cauavar <gabigoulartf@gmail.com>
2024-02-28 21:48:09 -0300

    commit 10

--- menu.js
+++ menu.js
@@ -1,7 +1,22 @@
 
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+let links = document.querySelectorAll('a');
+
+links.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        if (!link.classList.contains('img-port')) {
+            event.preventDefault();
+        } else {
+            return;
+        }
+
+        let targetId = this.getAttribute('href').substring(1);
+        let targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -14,3 +29,4 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
